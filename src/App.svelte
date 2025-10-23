8760fad492d290237bc3e316219b12e65a9be1e4
Richard Akinmade <111028004+ARichie1@users.noreply.github.com>
2025-10-23 20:51:53 +0100

    stores and import
    created store file and added component imports to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,19 @@
+<script>
+  import Header from './components/Header.svelte';
+  import UserTable from './components/UserTable.svelte';
+  import UserForm from './components/UserForm.svelte';
+  import Modal from './components/Modal.svelte';
+  import ConfirmDelete from './components/ConfirmDelete.svelte';
+  import Author from './components/Author.svelte';
+
+  // For Conditional Output
+  let showModal = $state(false);
+  let editing = $state(null);
+  let deleting = $state(false)
+  let userToDelete = $state({})
+  let searchQuery = $state('')
+
+
+
+</script>
 
