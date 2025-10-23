c8050f309ec3736fd2de23c1d4f806e5048b3bb2
Richard Akinmade <111028004+ARichie1@users.noreply.github.com>
2025-10-24 00:21:54 +0100

    fixing some css

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,6 +2,24 @@
   let {query = $bindable(), onSearch, onAdd} = $props()
   
   let search = $state()
+  let showMenu = $state(false)
+
+  let clientScreenSize = $state({
+    height: window.screen.height,
+    width: window.screen.width
+  })
+
+  // $effect(() => {
+  //   window.addEventListener("resize", (e) => {
+  //     clientScreenSize = {...clientScreenSize,
+  //       {width: window.screen.width, height: window.screen.height }
+  //     }
+  //      console.log(JSON.stringify(clientScreenSize));
+  //   })
+  // })
+  
+ 
+  
 
   // Dark / Light Toggle
   let theme = $state(localStorage.getItem('theme') || 'light');
@@ -26,11 +44,15 @@
     <h2>SVELTE ADMIN DASHBOARD</h2>
   </div>
 
-  <div class="header-right">
-    <input class="input" placeholder="Search..." bind:value={search} oninput={() => onSearch?.(search)} />
-    <button class="btn ghost small add-user" onclick={() => onAdd?.()}>+ Add User</button>
-    <button class="dark-toggle" onclick={toggleTheme}>{theme === 'light' ? '🌙' : '☀️'}</button>
-  </div>
+  {#if showMenu}
+    <div class="header-right">
+      <input class="input" placeholder="Search..." bind:value={search} oninput={() => onSearch?.(search)} />
+      <button class="btn ghost small add-user" onclick={() => onAdd?.()}>+ Add User</button>
+      <button class="dark-toggle" onclick={toggleTheme}>{theme === 'light' ? '🌙' : '☀️'}</button>
+    </div>
+  {/if}
+
+  <button class="menu-btn" onclick={() => showMenu = !showMenu}>📑</button>
 </div>
 
 <style>
@@ -53,5 +75,29 @@
         text-align: center;
         display: flex;
         justify-content: center;
+    }
+    .menu-btn{display: none ;}
+    @media screen and (max-width:600px){
+      .header-right {
+        position: absolute;
+        top: 12%; right: 0%;
+        width: 60%; height: 15rem;
+        display:flex; gap:8px;
+        align-items:center; justify-content: space-evenly;
+        flex-direction: column;
+        background: var(--accent);
+        border-bottom-left-radius: 6px;
+        transition: 1s ease-in-out;
+      }
+      .input, .add-user{
+        width:85%
+      }
+      .menu-btn{
+        display: flex ;
+        font-size: 20px;
+        cursor: pointer;
+        background: transparent;
+        border: none;
+      }
     }
 </style>
--- src/components/UserTable.svelte
+++ src/components/UserTable.svelte
@@ -8,28 +8,28 @@
   <div class="empty card">No users found.</div>
 {:else}
 <div class="table-container">
-  <table class="table card">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-      {#each (query ? $users.filter(user => user.name.toLowerCase().includes(query) || user.email.toLowerCase().includes(query)) : $users) as user (user.id)}
-        <tr>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td><span class="role" style="text-transform: uppercase;">{user.role}</span></td>
-          <td style="display:flex;gap:8px;">
-            <button class="btn ghost small" onclick={() => onEdit?.(user.id)}>Edit</button>
-            <button class="btn small delete" onclick={() => onDelete?.(user)}>Delete</button>
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-</table>
+    <table class="table card">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        {#each (query ? $users.filter(user => user.name.toLowerCase().includes(query) || user.email.toLowerCase().includes(query)) : $users) as user (user.id)}
+            <tr>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td><span class="role" style="text-transform: uppercase;">{user.role}</span></td>
+            <td style="display:flex;gap:8px;">
+                <button class="btn ghost small" onclick={() => onEdit?.(user.id)}>Edit</button>
+                <button class="btn small delete" onclick={() => onDelete?.(user)}>Delete</button>
+            </td>
+            </tr>
+        {/each}
+        </tbody>
+    </table>
 </div>
 {/if}
